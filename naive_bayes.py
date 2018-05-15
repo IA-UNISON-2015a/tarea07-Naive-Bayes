30aa1948d3675833e886c95130838289abd70b21
root <root@computerOne.localdomain>
2018-05-14 22:53:59 -0700

    Cambio naive_bayes y explicando resultados

--- naive_bayes.py
+++ naive_bayes.py
@@ -70,7 +70,7 @@ def main():
     print("----------------------------------------------")
 
     datos, clases = carga_archivo("dna.data")
-    clasificador = nb.NaiveBayes(range(len(datos[0])))
+    clasificador = nb.NaiveBayes()
 
     clasificador.aprende(datos, clases)
     clases_estimadas = clasificador.reconoce(datos)
@@ -88,7 +88,7 @@ def main():
     print("----------------------------------------------")
 
     datos, clases = carga_archivo("dna_noise.data")
-    clasificador_ruido = nb.NaiveBayes(range(len(datos[0])))
+    clasificador_ruido = nb.NaiveBayes()
 
     clasificador_ruido.aprende(datos, clases)
     clases_estimadas = clasificador_ruido.reconoce(datos)
@@ -103,3 +103,24 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    """
+    Resultados obtenidos:
+
+    Prueba con la base de datos de DNA sin ruido
+    ----------------------------------------------
+    Error de estimación en los mismos datos: 4.65 %
+    Error de estimación en los datos de prueba: 5.81787521079258 %
+
+
+    Prueba con la base de datos de DNA con ruido
+    ----------------------------------------------
+    Error de estimación en los mismos datos: 4.55%
+    Error de estimación en los datos de prueba: 5.902192242833052%
+
+
+    Los porcentajes de errores son similares para tanto los datos con ruido y sin ruido porque
+    los datos reales tienen más peso en el calculo y los datos con ruido no llegan afectar mucho
+    en el resultado.
+
+    """
